38bcdc78d472030e5409f86b7a7ce8afdeaef877
Dcode3244 <Dagimourgie111@gmail.com>
2022-12-12 18:17:14 +0300

    code cleanup

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,7 +1,5 @@
 #include "binary_trees.h"
 
-void traverse_right(const binary_tree_t *tree, void (*func)(int));
-void traverse_left(const binary_tree_t *tree, void (*func)(int));
 /**
  * binary_tree_preorder - goes through binary tree using
  * the pre-order traversal
@@ -15,40 +13,6 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 
 	func(tree->n);
-	if (tree->left)
-		traverse_left(tree->left, func);
-
-	if (tree->right)
-		traverse_right(tree->right, func);
-}
-
-/**
- * traverse_left - goes through left side of binary tree
- * @tree: the tree to be traversed
- * @func: pointer to a function to call for each node
- */
-void traverse_left(const binary_tree_t *tree, void (*func)(int))
-{
-	func(tree->n);
-
-	if (tree->left)
-		traverse_left(tree->left, func);
-	if (tree->right)
-		traverse_right(tree->right, func);
-
-}
-
-/**
- * traverse_right - goes through right side of binary tree
- * @tree: the tree to be traversed
- * @func: pointer to a function to call for each node
- */
-void traverse_right(const binary_tree_t *tree, void (*func)(int))
-{
-	func(tree->n);
-
-	if (tree->left)
-		traverse_left(tree->left, func);
-	if (tree->right)
-		traverse_right(tree->right, func);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
