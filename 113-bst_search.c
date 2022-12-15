ef981c7c585cb6cee1f623bdd9beb35d940b7449
Dcode3244 <Dagimourgie111@gmail.com>
2022-12-15 19:13:28 +0300

    advanced task 27

---
+++ 113-bst_search.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * bst_search - searches for a value in a Binary Search tree
+ * @tree: pointer to the root node of the BST to search
+ * @value: value to search in the tree
+ * Return: pointer to the node containin ga value equals to value
+ * or NULL if tree is NULL or if notting is found
+ */
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	if (!tree)
+		return (NULL);
+
+	if (tree->n == value)
+		return ((bst_t *)tree);
+	else if (tree->n > value)
+		return (bst_search(tree->left, value));
+	else
+		return (bst_search(tree->right, value));
+}
