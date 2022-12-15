dd200dd9f1f035086d1c58a9f57896fc77530fa9
Dcode3244 <Dagimourgie111@gmail.com>
2022-12-15 13:52:19 +0300

    advanced task 22

---
+++ 103-binary_tree_rotate_left.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_left - performs left rotation on a binary tree
+ * @tree: pointer to the root node of the tree to be ckecked
+ * Return: pointer to the new root node of the tree once rotated
+ */
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	binary_tree_t *r;
+
+	if (tree == NULL)
+		return (NULL);
+	r = tree->right;
+
+	r->parent = tree->parent;
+	tree->right = r->left;
+	tree->parent = r;
+	r->left = tree;
+	if (r->parent)
+	{
+		if (r->parent->left == tree)
+			r->parent->left = r;
+		else
+			r->parent->right = r;
+	}
+	return (r);
+}
